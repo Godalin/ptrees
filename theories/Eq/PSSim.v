1abd9411860b89855250fedaeeef8a810f4e1d3c
Linyu Yang <yly1228@foxmail.com>
2025-03-28 14:15:35 +0800

    added: pssim_cond characteristic

--- theories/Eq/PSSim.v
+++ theories/Eq/PSSim.v
@@ -18,6 +18,8 @@ From PTree.Eq Require Import ShallowNew EquNew Trans.
 Section PSSim.
 Import Enum.
 Import NonnegQNotations.
+Import GRing.Theory Order.Theory.
+Import EquNotations.
 
 #[local] Notation ptree E := (ptree E Enum).
 #[local] Notation ptree' E R := (ptree' E Enum R).
@@ -54,83 +56,6 @@ Definition enumk {Y} {X : eqType} (μ : Enum X) (k : X → Y) (x : X) : ℚ≥0 
 
 Section experiment.
 Context {E F : Type → Type} {X Y : Type}.
-
-(** Goal: define a prop: [R t u] is a simulation, or part of the prop
-    t -- R ---- u
-    |         | | |
-  l |         p q r l
-    |         | | |
-    v         v v v
-    t'-- R -- u'u'u'
-  *)
-
-Variant pssim_cond REL (t' : ptree E X) (l' : label) (p : ℚ≥0)
-  : ptree' F Y → Prop :=
-  | PSSimRetF
-    : ∀ r u',
-        trans l' p (Ret r) u'
-      → pssim_cond REL t' l' p (RetF r)
-  | PSSimTauF
-    : ∀ t u',
-        trans l' p (Tau t) u'
-      → pssim_cond REL t' l' p (TauF t)
-  | PSSimVisF
-    : ∀ X' (e : F X') k u',
-        trans l' p (Vis e k) u'
-      → pssim_cond REL t' l' p (VisF e k)
-  | PSSimProbF
-    : ∀ (X' : eqType) (μ : Enum X') (k : X' → ptree F Y) (s : seq X'),
-        subseq s (supp μ)
-      → transAll l' (Prob μ k) [seq enumk μ k x | x <- s]
-      → relateAll REL t' [seq k x | x <- s]
-      → p <= mass μ s
-      → pssim_cond REL t' l' p (ProbF μ k).
-
-(* Variant pssim_cond REL (t' : ptree E X) (l' : label) (p : ℚ≥0) *)
-(*   : ptree' F Y → Prop := *)
-(*   | PSSimTrans' *)
-(*     : ∀ u u', *)
-(*         trans l' p u u' *)
-(*       → pssim_cond REL t' l' p (observe u) *)
-(*   | PSSimProbF' *)
-(*     : ∀ (X' : eqType) (μ : Enum X') (k : X' → ptree F Y) (s : seq X'), *)
-(*         subseq s (supp μ) *)
-(*       → transAll l' (Prob μ k) [seq enumk μ k x | x <- s] *)
-(*       → relateAll REL t' [seq k x | x <- s] *)
-(*       → p <= mass μ s *)
-(*       → pssim_cond REL t' l' p (ProbF μ k). *)
-
-Lemma sub_pssim_cond (R S : rel (ptree E X) (ptree F Y))
-    t' (l : @label F) p u
-  : (∀ x y, R x y → S x y) → pssim_cond R t' l p u → pssim_cond S t' l p u.
-Proof. intros. inversion H0.
-  - econstructor. apply H1.
-  - econstructor. apply H1.
-  - econstructor. apply H1.
-  - econstructor. apply H1. all: auto.
-    apply (sub_relateAll R S); auto.
-Qed.
-
-
-
-End experiment.
-
-(** [pss] is the monotone function for probabilistic strong simulation  *)
-
-#[program]
-Definition pss {E F : Type → Type}
-    {X Y : Type} (L : rel (@label E) (@label F))
-  : mon (ptree E X → ptree F Y → Prop)
-  := {| body R t u := ∀ l (p : ℚ≥0) t',
-        trans l p t t' → ∃ l',
-          L l l' ∧ pssim_cond R t' l' p (observe u)
-     |}.
-Next Obligation.
-  destruct (H0 l p t' H1) as [l' [RL HCond]].
-  exists l'. split; auto. apply (sub_pssim_cond x y); auto.
-Defined.
-
-
 
 (* Old definitions of [pss], probabilistic strong simulation.
   This version has the problem of not being unique in the provided
@@ -169,6 +94,106 @@ Defined.
 (* exists l', X', k', x's. repeat (split; auto). *)
 (* apply: sub_relateAll. apply H. auto. *)
 (* Defined. *)
+
+(** Goal: define a prop: [R t u] is a simulation, or part of the prop
+    t -- R ---- u
+    |         | | |
+  l |         p q r l
+    |         | | |
+    v         v v v
+    t'-- R -- u'u'u'
+  *)
+
+Variant pssim_cond REL (t' : ptree E X) (l' : label) (p : ℚ≥0)
+  : ptree' F Y → Prop :=
+  | PSSimRetF
+    : ∀ r u',
+        trans l' p (Ret r) u'
+      → REL t' u'
+      → pssim_cond REL t' l' p (RetF r)
+  | PSSimTauF
+    : ∀ t u',
+        trans l' p (Tau t) u'
+      → REL t' u'
+      → pssim_cond REL t' l' p (TauF t)
+  | PSSimVisF
+    : ∀ X' (e : F X') k u',
+        trans l' p (Vis e k) u'
+      → REL t' u'
+      → pssim_cond REL t' l' p (VisF e k)
+  | PSSimProbF
+    : ∀ (X' : eqType) (μ : Enum X') (k : X' → ptree F Y) (s : seq X'),
+        subseq s (supp μ)
+      → transAll l' (Prob μ k) [seq enumk μ k x | x <- s]
+      → relateAll REL t' [seq k x | x <- s]
+      → p <= mass μ s
+      → pssim_cond REL t' l' p (ProbF μ k).
+
+(* Variant pssim_cond REL (t' : ptree E X) (l' : label) (p : ℚ≥0) *)
+(*   : ptree' F Y → Prop := *)
+(*   | PSSimTrans' *)
+(*     : ∀ u u', *)
+(*         trans l' p u u' *)
+(*       → pssim_cond REL t' l' p (observe u) *)
+(*   | PSSimProbF' *)
+(*     : ∀ (X' : eqType) (μ : Enum X') (k : X' → ptree F Y) (s : seq X'), *)
+(*         subseq s (supp μ) *)
+(*       → transAll l' (Prob μ k) [seq enumk μ k x | x <- s] *)
+(*       → relateAll REL t' [seq k x | x <- s] *)
+(*       → p <= mass μ s *)
+(*       → pssim_cond REL t' l' p (ProbF μ k). *)
+
+Lemma sub_pssim_cond (R S : rel (ptree E X) (ptree F Y))
+    t' (l : @label F) p u
+  : (∀ x y, R x y → S x y) → pssim_cond R t' l p u → pssim_cond S t' l p u.
+Proof. intros. inversion H0.
+  - econstructor. apply H1. auto.
+  - econstructor. apply H1. auto.
+  - econstructor. apply H1. auto.
+  - econstructor. apply H1. all: auto.
+    apply (sub_relateAll R S); auto.
+Qed.
+
+Lemma pssim_cond_char : ∀ (REL : rel (ptree E X) (ptree F Y)) t' l p u,
+    pssim_cond REL t' l p u
+  → ∃ (u's : Enum (ptree F Y)),
+      transAll l (go u) u's
+    ∧ relateAll REL t' [seq snd p | p <- u's]
+    ∧ p <= transAllPrb u's.
+Proof. intros REL t' l p u H. inversion H; subst.
+  - exists [:: (p, u')]; repeat split; simpl.
+    inversion H0; subst; auto. assumption.
+    rewrite addr0. rewrite le_refl. auto.
+  - exists [:: (p, u')]; repeat split; simpl.
+    inversion H0; subst; auto. assumption.
+    rewrite addr0. rewrite le_refl. auto.
+  - exists [:: (p, u')]; repeat split; simpl.
+    inversion H0; subst. dependent destruction H7.
+    econstructor. rewrite -> H5. apply observe_equ_eq.
+    assumption. assumption.
+    rewrite addr0. rewrite le_refl. auto.
+  - exists (map (enumk μ k) s); repeat split; simpl.
+    apply H1.
+Admitted.
+
+
+
+End experiment.
+
+(** [pss] is the monotone function for probabilistic strong simulation  *)
+
+#[program]
+Definition pss {E F : Type → Type}
+    {X Y : Type} (L : rel (@label E) (@label F))
+  : mon (ptree E X → ptree F Y → Prop)
+  := {| body R t u := ∀ l (p : ℚ≥0) t',
+        trans l p t t' → ∃ l',
+          L l l' ∧ pssim_cond R t' l' p (observe u)
+     |}.
+Next Obligation.
+  destruct (H0 l p t' H1) as [l' [RL HCond]].
+  exists l'. split; auto. apply (sub_pssim_cond x y); auto.
+Defined.
 
 (** [pssim] is the probabilistic similarity, which is defined as
     the greatest fixpoint of the monotone function [pss] *)
