b030ad99b5dce3bc187550ba8dbd3f17c63b9cdf
Linyu Yang <yly1228@foxmail.com>
2026-08-09 20:15:08 +0800

    feat(prob): add Enum expectation calculus

--- theories/Prob/MeasureIterationEnum.v
+++ theories/Prob/MeasureIterationEnum.v
@@ -29,9 +29,10 @@ Fixpoint enum_expect {A} (f : A -> rat) (mu : Enum A) : rat :=
     relational limit: a chain may converge mathematically while its limit is
     not representable by a finite rational [Enum]. *)
 Definition enum_converges {A} (chain : nat -> Enum A) (mu : Enum A) : Prop :=
-  forall f : A -> rat, forall eps : rat, 0 < eps ->
+  forall P : A -> bool, forall eps : rat, 0 < eps ->
     exists N, forall n, (N <= n)%nat ->
-      `|enum_expect f (chain n) - enum_expect f mu| < eps.
+      `|enum_expect (fun x => if P x then 1 else 0) (chain n) -
+        enum_expect (fun x => if P x then 1 else 0) mu| < eps.
 
 #[global] Instance Enum_MeasureOmegaInterface :
     @MeasureOmegaInterface Enum Enum_MeasureInterface := {
@@ -51,6 +52,30 @@ Proof. reflexivity. Qed.
 Lemma enum_expect_ret {A} (f : A -> rat) x :
   enum_expect f (ret_Enum x) = f x.
 Proof. by rewrite /ret_Enum /= mul1r addr0. Qed.
+
+Lemma enum_expect_app {A} (f : A -> rat) (mu nu : Enum A) :
+  enum_expect f (mu ++ nu) = enum_expect f mu + enum_expect f nu.
+Proof.
+  elim: mu=> [|[p x] mu IH] /=; first by rewrite add0r.
+  by rewrite IH addrA.
+Qed.
+
+Lemma enum_expect_scale {A} (f : A -> rat) p (mu : Enum A) :
+  enum_expect f (scale_Enum p mu) = Qval p * enum_expect f mu.
+Proof.
+  elim: mu=> [|[q x] mu IH] /=; first by rewrite mulr0.
+  rewrite IH mulrDr. congr (_ + _).
+  by rewrite !mulrA.
+Qed.
+
+Lemma enum_expect_bind {A B} (f : B -> rat)
+    (mu : Enum A) (k : A -> Enum B) :
+  enum_expect f (bind_Enum mu k) =
+    enum_expect (fun x => enum_expect f (k x)) mu.
+Proof.
+  elim: mu=> [|[p x] mu IH] //=.
+  by rewrite enum_expect_app enum_expect_scale IH.
+Qed.
 
 (** This interface intentionally has no global [MeasureOmegaLaws] instance:
     observational uniqueness of limits does not imply the current
