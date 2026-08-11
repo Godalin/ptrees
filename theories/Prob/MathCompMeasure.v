25a2f6aea460e5ed833d468e1488bf768326f73c
Linyu Yang <yly1228@foxmail.com>
2026-08-11 22:41:06 +0800

    define mathcomp measure coupling foundation

--- theories/Prob/MathCompMeasure.v
+++ theories/Prob/MathCompMeasure.v
@@ -6,9 +6,13 @@ From mathcomp Require Import all_ssreflect all_algebra.
 From mathcomp Require Import boolp classical_sets functions cardinality reals.
 From mathcomp.analysis Require Import measure probability kernel.
 
+From PTree.Prob Require Import FrontierLift.
+
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
+
+Local Open Scope classical_set_scope.
 
 (** A uniform discrete measurable carrier for an arbitrary Coq type.  The
     extra [MCBottom] point is needed because MathComp's [measurableType]
@@ -44,5 +48,48 @@ Definition mathcomp_measure_set (A : Type) :=
 Definition mathcomp_measure_ret {A} (x : A) :
     subprobability (mc_carrier A) R :=
   [the subprobability (mc_carrier A) R of dirac (MCValue x)].
+
+(** Extensional equality on measurable sets. *)
+Definition mathcomp_measure_eq {A}
+    (mu nu : subprobability (mc_carrier A) R) : Prop :=
+  forall U : set (mc_carrier A), measurable U -> mu U = nu U.
+
+(** A predicate on returned values is lifted to the carrier by declaring
+    the bookkeeping bottom point valid.  Consequently missing mass never
+    falsifies an almost-everywhere assertion about returned values. *)
+Definition mc_predicate {A} (P : A -> Prop) : set (mc_carrier A) :=
+  [set x | match x with MCBottom => True | MCValue a => P a end].
+
+Definition mathcomp_measure_ae {A}
+    (mu : subprobability (mc_carrier A) R) (P : A -> Prop) : Prop :=
+  almost_everywhere mu (mc_predicate P).
+
+(** Relations used by couplings also relate the two bottom points.  A
+    one-sided bottom point is deliberately unrelated: it would represent a
+    mismatch in lost mass. *)
+Definition mc_relation {A B} (rel : A -> B -> Prop) :
+    set (mc_carrier A * mc_carrier B) :=
+  [set xy | match xy with
+   | (MCBottom, MCBottom) => True
+   | (MCValue a, MCValue b) => rel a b
+   | _ => False
+   end].
+
+Definition mathcomp_coupling {A B} (rel : A -> B -> Prop)
+    (mu : subprobability (mc_carrier A) R)
+    (nu : subprobability (mc_carrier B) R) : Prop :=
+  exists joint : subprobability (mc_carrier A * mc_carrier B)%type R,
+    (forall U : set (mc_carrier A), measurable U ->
+      joint (fst @^-1` U) = mu U) /\
+    (forall V : set (mc_carrier B), measurable V ->
+      joint (snd @^-1` V) = nu V) /\
+    almost_everywhere joint (mc_relation rel).
+
+(** The intended [MeasureInterface] lifting is existence of a subprobability
+    coupling concentrated almost everywhere on the lifted relation. *)
+Definition mathcomp_measure_lift {A B} (rel : A -> B -> Prop)
+    (mu : subprobability (mc_carrier A) R)
+    (nu : subprobability (mc_carrier B) R) : Prop :=
+  mathcomp_coupling rel mu nu.
 
 End BackendShape.
