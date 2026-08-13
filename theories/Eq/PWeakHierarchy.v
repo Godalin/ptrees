45b2c7a339436ee1aacb5a4f32c4dba594be96ad
Linyu Yang <yly1228@foxmail.com>
2026-08-14 00:31:52 +0800

    derive finite weak bisimulation from pstrong

--- theories/Eq/PWeakHierarchy.v
+++ theories/Eq/PWeakHierarchy.v
@@ -2,6 +2,8 @@ Set Warnings "-notation-overridden".
 Set Warnings "-ambiguous-paths".
 
 Require Import Utf8.
+Require Import ClassicalEpsilon.
+Require Import Program.Equality.
 
 From Coinduction Require Import all.
 
@@ -116,22 +118,113 @@ End FiniteToASTInclusion.
 
 Section StrongToFiniteWeak.
 Context {E : Type -> Type} {M : Type -> Type}
-  `{MI : MeasureInterface M} `{MC : @MeasureCoreLaws M MI}.
+  `{MI : MeasureInterface M} `{MZ : MeasureZeroInterface M}
+  `{MC : @MeasureCoreLaws M MI}
+  `{ML : @MeasureLaws M MI MC} `{MB : @MeasureBindLaws M MI}
+  `{MAE : @MeasureLiftAELaws M MI}
+  `{MO : @MeasureOmegaInterface M MI}.
 
-(** Strong-to-frontier coherence is the precise obligation needed in the
-    probabilistic constructor: a coupling of residual states must lift to
-    matching couplings of every finite stable frontier.  General measure
-    interfaces need an AE selection/relational Kleisli principle to prove
-    this; it is therefore kept explicit rather than hidden in [pstrong]. *)
-Class PStrongFrontierLaws := {
-  pstrong_frontier_match : forall {R1 R2}
-      (RR : R1 -> R2 -> Prop)
-      (t1 : ptree E M R1) (t2 : ptree E M R2),
-      pstrong RR t1 t2 ->
-      apfrontier_match RR (pstrong RR) (observe t1) (observe t2)
-}.
+Lemma pstrongF_apfrontier_transport {R1 R2}
+    (RR : R1 -> R2 -> Prop) :
+  forall (ot1 : ptree' E M R1) hs1,
+    apfrontier ot1 hs1 ->
+    forall (ot2 : ptree' E M R2),
+      pstrongF RR (pstrong RR) ot1 ot2 ->
+      exists hs2, apfrontier ot2 hs2 /\
+        meas_lift (aphead_rel RR (pstrong RR)) hs1 hs2.
+Proof.
+  intros ot1 hs1 Hf1. induction Hf1; intros ot2 Hstrong;
+    dependent destruction Hstrong.
+  - exists (meas_ret (APHRet r2)). split; first constructor.
+    apply meas_lift_ret. constructor. assumption.
+  - exists (meas_ret (APHVis e k2)). split; first constructor.
+    apply meas_lift_ret. constructor. assumption.
+  - destruct (IHHf1 _ (pstrong_unfold H)) as [hs2 [Hf2 Hlift]].
+    exists hs2. split; [exact (APFTau Hf2)|exact Hlift].
+  - pose (Good2 y := exists x,
+      pstrong RR (k x) (k2 y) /\ Good x).
+    assert (Hgood2 : meas_ae nu Good2).
+    { unfold Good2. eapply meas_lift_ae_transport_r; eassumption. }
+    assert (Hex : forall y, Good2 y -> exists hs,
+        apfrontier (observe (k2 y)) hs /\
+        exists x, pstrong RR (k x) (k2 y) /\ Good x /\
+          meas_lift (aphead_rel RR (pstrong RR)) (front x) hs).
+    { intros y [x [Hxy Hgx]].
+      match goal with IH : forall x, Good x -> _ |- _ =>
+        destruct (IH x Hgx _ (pstrong_unfold Hxy))
+          as [hs [Hfs Hlift]] end.
+      exists hs. split; [exact Hfs|].
+      exists x. repeat split; assumption. }
+    pose (front2 y :=
+      match excluded_middle_informative (Good2 y) with
+      | left Hy => proj1_sig (constructive_indefinite_description _ (Hex y Hy))
+      | right _ => @meas_empty M MZ _
+      end).
+    exists (meas_bind nu front2). split.
+    + apply (APFProb (front := front2) (Good := Good2)).
+      * exact Hgood2.
+      *
+      intros y Hy. unfold front2.
+      destruct excluded_middle_informative as [Hy'|Hn]; last contradiction.
+      exact (proj1 (proj2_sig
+        (constructive_indefinite_description _ (Hex y Hy')))).
+    + eapply meas_lift_bind_ae
+        with (R := fun x y => pstrong RR (k x) (k2 y))
+             (P := Good) (Q := Good2).
+      * exact H2.
+      * exact H.
+      * exact Hgood2.
+      *
+      intros x y Hxy Hgx Hgy. unfold front2.
+      destruct excluded_middle_informative as [Hy|Hn]; last contradiction.
+      destruct (proj2 (proj2_sig
+        (constructive_indefinite_description _ (Hex y Hy))))
+        as [z [Hzy [Hgz Hz]]].
+      match goal with IH : forall x, Good x -> _ |- _ =>
+        destruct (IH x Hgx _ (pstrong_unfold Hxy))
+          as [hsx [Hfsx Hliftx]] end.
+      assert (Heq : meas_eq hsx
+          (proj1_sig (constructive_indefinite_description _ (Hex y Hy)))).
+      { apply (apfrontier_sem_unique (ot := observe (k2 y))).
+        - exists hsx. split; [exact Hfsx|apply meas_eq_refl].
+        - exists (proj1_sig
+            (constructive_indefinite_description _ (Hex y Hy))). split.
+          + exact (proj1 (proj2_sig
+              (constructive_indefinite_description _ (Hex y Hy)))).
+          + apply meas_eq_refl. }
+      eapply meas_lift_proper_r; [exact Heq|exact Hliftx].
+Qed.
 
-Context `{PSF : PStrongFrontierLaws}.
+Lemma pstrong_apfrontier_transport {R1 R2}
+    (RR : R1 -> R2 -> Prop) (t1 : ptree E M R1) (t2 : ptree E M R2) :
+  pstrong RR t1 t2 ->
+  forall hs1, apfrontier (observe t1) hs1 ->
+  exists hs2, apfrontier (observe t2) hs2 /\
+    meas_lift (aphead_rel RR (pstrong RR)) hs1 hs2.
+Proof.
+  intros Hstrong hs1 Hf1.
+  exact (@pstrongF_apfrontier_transport R1 R2 RR
+    (observe t1) hs1 Hf1 (observe t2) (pstrong_unfold Hstrong)).
+Qed.
+
+Theorem pstrong_frontier_match {R1 R2}
+    (RR : R1 -> R2 -> Prop)
+    (t1 : ptree E M R1) (t2 : ptree E M R2) :
+  pstrong RR t1 t2 ->
+  apfrontier_match RR (pstrong RR) (observe t1) (observe t2).
+Proof.
+  intros Hstrong. split.
+  - exact (pstrong_apfrontier_transport Hstrong).
+  - intros hs2 Hf2.
+    pose proof (pstrong_sym Hstrong) as Hsym.
+    destruct (pstrong_apfrontier_transport Hsym Hf2)
+      as [hs1 [Hf1 Hlift]].
+    exists hs1. split; [exact Hf1|].
+    eapply meas_lift_mono; [|exact (meas_lift_sym Hlift)].
+    intros h1 h2 Hh. dependent destruction Hh.
+    + constructor. exact H.
+    + constructor. intro x. apply pstrong_sym. exact (H x).
+Qed.
 
 Theorem pstrong_apweak {R1 R2} (RR : R1 -> R2 -> Prop) :
   forall (t1 : ptree E M R1) (t2 : ptree E M R2),
@@ -161,6 +254,22 @@ Proof.
     + eapply apfrontier_match_mono; [exact CIH|].
       rewrite H. rewrite H0. exact (pstrong_frontier_match Hstrong).
     + eapply meas_lift_mono; [|exact H1]. intros x y Hxy. exact (CIH _ _ Hxy).
+Qed.
+
+Corollary pstrong_auweak_of_finite_generation {R1 R2}
+    (RR : R1 -> R2 -> Prop)
+    (finite_generation : forall (t1 : ptree E M R1)
+      (t2 : ptree E M R2),
+      apweak RR t1 t2 ->
+      aufrontier_finitely_generated (observe t1) /\
+      aufrontier_finitely_generated (observe t2)) :
+  forall (t1 : ptree E M R1) (t2 : ptree E M R2),
+    pstrong RR t1 t2 -> auweak RR t1 t2.
+Proof.
+  intros t1 t2 Hstrong.
+  apply (@apweak_auweak_of_finite_generation E M MI MC ML MO
+    R1 R2 RR finite_generation t1 t2).
+  exact (@pstrong_apweak R1 R2 RR t1 t2 Hstrong).
 Qed.
 
 End StrongToFiniteWeak.
