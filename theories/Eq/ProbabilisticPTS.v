65d90d47d927f68a8a6072285addd3f980cdd54c
Linyu Yang <yly1228@foxmail.com>
2026-08-13 13:24:24 +0800

    characterize auweak by guarded PTS bisimulation

--- theories/Eq/ProbabilisticPTS.v
+++ theories/Eq/ProbabilisticPTS.v
@@ -301,6 +301,20 @@ Lemma ppts_bisim_fold t1 t2 :
   ppts_bisimF ppts_bisim t1 t2 -> ppts_bisim t1 t2.
 Proof. intro H. unfold ppts_bisim. apply (gfp_fp fppts_bisim). exact H. Qed.
 
+(** The weak-transition-only relation is intentionally termination
+    observational: if neither side has any AST weak transition, its matching
+    clauses are vacuous.  This theorem records the exact reason unrestricted
+    completeness to guarded [auweak] cannot hold. *)
+Lemma ppts_bisim_of_no_weak_transitions t1 t2 :
+  (forall hs, ~ ppts_ast_weak (observe t1) hs) ->
+  (forall hs, ~ ppts_ast_weak (observe t2) hs) ->
+  ppts_bisim t1 t2.
+Proof.
+  intros Hnone1 Hnone2. apply ppts_bisim_fold. split.
+  - intros hs Hweak. exfalso. exact (Hnone1 _ Hweak).
+  - intros hs Hweak. exfalso. exact (Hnone2 _ Hweak).
+Qed.
+
 End PTSBisimulation.
 
 Section AuweakPTSSoundness.
@@ -341,3 +355,258 @@ Proof.
 Qed.
 
 End AuweakPTSSoundness.
+
+Section PTSCompleteness.
+Context {E : Type -> Type} {M : Type -> Type}
+  `{MI : MeasureInterface M} `{MC : @MeasureCoreLaws M MI}
+  `{ML : @MeasureLaws M MI MC} `{MB : @MeasureBindLaws M MI}
+  `{MM : @MeasureMonadLaws M MI}
+  `{MG : @MeasureCongruenceLaws M MI}
+  `{MO : @MeasureOmegaInterface M MI}.
+
+Theorem ppts_bisim_complete_productive {R1 R2}
+    (RR : R1 -> R2 -> Prop)
+    (productive : forall u v,
+      @ppts_bisim E M MI MC MO R1 R2 RR u v ->
+      exists hs, ppts_ast_weak (observe u) hs) :
+  forall t1 t2,
+    @ppts_bisim E M MI MC MO R1 R2 RR t1 t2 ->
+    @auweak E M MI MC MO R1 R2 RR t1 t2.
+Proof.
+  unfold auweak. coinduction CH CIH. intros t1 t2 Hbisim.
+  unfold fauweak, auweak_body; cbn.
+  destruct (productive _ _ Hbisim) as [hs1 Hweak1].
+  destruct (ppts_bisim_unfold Hbisim) as [HL HR].
+  destruct (HL _ Hweak1) as [hs2 [Hweak2 Hlift]].
+  eapply AUWFrontier with (hs1 := hs1) (hs2 := hs2).
+  - exact (ppts_ast_weak_to_aufrontier Hweak1).
+  - exact (ppts_ast_weak_to_aufrontier Hweak2).
+  - eapply meas_lift_mono; [|exact Hlift].
+    intros h1 h2 Hh. dependent destruction Hh.
+    + constructor. assumption.
+    + constructor. intro x.
+      apply CIH. auto.
+Qed.
+
+End PTSCompleteness.
+
+Section GuardedPTSBisimulation.
+Context {E : Type -> Type} {M : Type -> Type}
+  `{MI : MeasureInterface M} `{MC : @MeasureCoreLaws M MI}
+  `{MO : @MeasureOmegaInterface M MI}.
+Context {R1 R2 : Type}.
+Variable RR : R1 -> R2 -> Prop.
+
+Definition ppts_ast_match
+    (sim : ptree E M R1 -> ptree E M R2 -> Prop)
+    (ot1 : ptree' E M R1) (ot2 : ptree' E M R2) : Prop :=
+  (forall hs1, ppts_ast_weak ot1 hs1 -> exists hs2,
+      ppts_ast_weak ot2 hs2 /\ meas_lift (aphead_rel RR sim) hs1 hs2) /\
+  (forall hs2, ppts_ast_weak ot2 hs2 -> exists hs1,
+      ppts_ast_weak ot1 hs1 /\ meas_lift (aphead_rel RR sim) hs1 hs2).
+
+(** Divergence-sensitive PTS bisimulation.  The frontier rule observes an
+    actual AST weak transition; the guarded Tau/Prob rules retain primitive
+    residual transitions when no such transition exists. *)
+Inductive ppts_guardedF
+    (sim : ptree E M R1 -> ptree E M R2 -> Prop) :
+    ptree' E M R1 -> ptree' E M R2 -> Prop :=
+  | PPTSGFrontier ot1 ot2 hs1 hs2 :
+      ppts_ast_weak ot1 hs1 -> ppts_ast_weak ot2 hs2 ->
+      meas_lift (aphead_rel RR sim) hs1 hs2 ->
+      ppts_guardedF sim ot1 ot2
+  | PPTSGTau t1 t2 :
+      ppts_ast_match sim (TauF t1) (TauF t2) ->
+      sim t1 t2 -> ppts_guardedF sim (TauF t1) (TauF t2)
+  | PPTSGProb {X Y} (mu : M X) (nu : M Y) k1 k2 :
+      ppts_ast_match sim (ProbF mu k1) (ProbF nu k2) ->
+      meas_lift (fun x y => sim (k1 x) (k2 y)) mu nu ->
+      ppts_guardedF sim (ProbF mu k1) (ProbF nu k2)
+  | PPTSGTauL t1 ot2 :
+      ppts_guardedF sim (observe t1) ot2 ->
+      ppts_guardedF sim (TauF t1) ot2
+  | PPTSGTauR ot1 t2 :
+      ppts_guardedF sim ot1 (observe t2) ->
+      ppts_guardedF sim ot1 (TauF t2).
+
+Lemma ppts_ast_match_mono sim1 sim2 ot1 ot2 :
+  (forall t1 t2, sim1 t1 t2 -> sim2 t1 t2) ->
+  ppts_ast_match sim1 ot1 ot2 -> ppts_ast_match sim2 ot1 ot2.
+Proof.
+  intros Hsim [HL HR]. split; intros hs Hweak.
+  - destruct (HL _ Hweak) as [hs' [Hw' Hlift]]. exists hs'. split; auto.
+    eapply meas_lift_mono; [|exact Hlift]. exact (aphead_rel_mono Hsim).
+  - destruct (HR _ Hweak) as [hs' [Hw' Hlift]]. exists hs'. split; auto.
+    eapply meas_lift_mono; [|exact Hlift]. exact (aphead_rel_mono Hsim).
+Qed.
+
+Lemma ppts_guardedF_monotone sim1 sim2 :
+  (forall t1 t2, sim1 t1 t2 -> sim2 t1 t2) ->
+  forall ot1 ot2, ppts_guardedF sim1 ot1 ot2 ->
+    ppts_guardedF sim2 ot1 ot2.
+Proof.
+  intros Hsim ot1 ot2 Hstep. induction Hstep.
+  - eapply PPTSGFrontier; [exact H|exact H0|].
+    eapply meas_lift_mono; [|exact H1]. exact (aphead_rel_mono Hsim).
+  - apply PPTSGTau; [exact (ppts_ast_match_mono Hsim H)|exact (Hsim _ _ H0)].
+  - apply PPTSGProb; [exact (ppts_ast_match_mono Hsim H)|].
+    eapply meas_lift_mono; [|exact H0]. intros x y Hxy. exact (Hsim _ _ Hxy).
+  - exact (PPTSGTauL IHHstep).
+  - exact (PPTSGTauR IHHstep).
+Qed.
+
+Definition ppts_guarded_body sim (t1 : ptree E M R1)
+    (t2 : ptree E M R2) :=
+  ppts_guardedF sim (observe t1) (observe t2).
+
+Program Definition fppts_guarded :
+    mon (ptree E M R1 -> ptree E M R2 -> Prop) :=
+  {| body := ppts_guarded_body |}.
+Next Obligation.
+  intros sim1 sim2 Hsub t1 t2 H.
+  eapply ppts_guardedF_monotone; eauto.
+Qed.
+
+Definition ppts_guarded_bisim :
+    ptree E M R1 -> ptree E M R2 -> Prop := gfp fppts_guarded.
+
+Lemma ppts_guarded_unfold t1 t2 :
+  ppts_guarded_bisim t1 t2 ->
+  ppts_guardedF ppts_guarded_bisim (observe t1) (observe t2).
+Proof. intro H. apply (gfp_pfp fppts_guarded) in H. exact H. Qed.
+
+Lemma ppts_guarded_fold t1 t2 :
+  ppts_guardedF ppts_guarded_bisim (observe t1) (observe t2) ->
+  ppts_guarded_bisim t1 t2.
+Proof.
+  intro H. unfold ppts_guarded_bisim. apply (gfp_fp fppts_guarded). exact H.
+Qed.
+
+End GuardedPTSBisimulation.
+
+Section GuardedPTSCharacterization.
+Context {E : Type -> Type} {M : Type -> Type}
+  `{MI : MeasureInterface M} `{MC : @MeasureCoreLaws M MI}
+  `{ML : @MeasureLaws M MI MC} `{MB : @MeasureBindLaws M MI}
+  `{MM : @MeasureMonadLaws M MI}
+  `{MG : @MeasureCongruenceLaws M MI}
+  `{MO : @MeasureOmegaInterface M MI}.
+
+Lemma aufrontier_match_to_ppts_ast_match {R1 R2}
+    (RR : R1 -> R2 -> Prop)
+    (sim : ptree E M R1 -> ptree E M R2 -> Prop)
+    (ot1 : ptree' E M R1) (ot2 : ptree' E M R2) :
+  aufrontier_match RR sim ot1 ot2 -> ppts_ast_match RR sim ot1 ot2.
+Proof.
+  intros [HL HR]. split; intros hs Hpts.
+  - destruct (HL _ (ppts_ast_weak_to_aufrontier Hpts))
+      as [hs' [Hf' Hlift]].
+    exists hs'. split; [exact (aufrontier_to_ppts_ast_weak Hf')|exact Hlift].
+  - destruct (HR _ (ppts_ast_weak_to_aufrontier Hpts))
+      as [hs' [Hf' Hlift]].
+    exists hs'. split; [exact (aufrontier_to_ppts_ast_weak Hf')|exact Hlift].
+Qed.
+
+Lemma ppts_ast_match_to_aufrontier_match {R1 R2}
+    (RR : R1 -> R2 -> Prop)
+    (sim : ptree E M R1 -> ptree E M R2 -> Prop)
+    (ot1 : ptree' E M R1) (ot2 : ptree' E M R2) :
+  ppts_ast_match RR sim ot1 ot2 -> aufrontier_match RR sim ot1 ot2.
+Proof.
+  intros [HL HR]. split; intros hs Hf.
+  - destruct (HL _ (aufrontier_to_ppts_ast_weak Hf))
+      as [hs' [Hw' Hlift]].
+    exists hs'. split; [exact (ppts_ast_weak_to_aufrontier Hw')|exact Hlift].
+  - destruct (HR _ (aufrontier_to_ppts_ast_weak Hf))
+      as [hs' [Hw' Hlift]].
+    exists hs'. split; [exact (ppts_ast_weak_to_aufrontier Hw')|exact Hlift].
+Qed.
+
+Lemma aufrontier_match_sim_mono {R1 R2}
+    (RR : R1 -> R2 -> Prop)
+    (sim1 sim2 : ptree E M R1 -> ptree E M R2 -> Prop) ot1 ot2 :
+  (forall t1 t2, sim1 t1 t2 -> sim2 t1 t2) ->
+  aufrontier_match RR sim1 ot1 ot2 -> aufrontier_match RR sim2 ot1 ot2.
+Proof.
+  intros Hsim [HL HR]. split; intros hs Hf.
+  - destruct (HL _ Hf) as [hs' [Hf' Hlift]]. exists hs'. split; auto.
+    eapply meas_lift_mono; [|exact Hlift]. exact (aphead_rel_mono Hsim).
+  - destruct (HR _ Hf) as [hs' [Hf' Hlift]]. exists hs'. split; auto.
+    eapply meas_lift_mono; [|exact Hlift]. exact (aphead_rel_mono Hsim).
+Qed.
+
+Theorem auweak_to_ppts_guarded {R1 R2}
+    (RR : R1 -> R2 -> Prop) :
+  forall t1 t2,
+    @auweak E M MI MC MO R1 R2 RR t1 t2 ->
+    @ppts_guarded_bisim E M MI MC MO R1 R2 RR t1 t2.
+Proof.
+  unfold ppts_guarded_bisim. coinduction CH CIH.
+  intros t1 t2 Hrel. unfold fppts_guarded, ppts_guarded_body; cbn.
+  pose proof (auweak_unfold Hrel) as Hstep.
+  induction Hstep.
+  - eapply PPTSGFrontier.
+    + exact (aufrontier_to_ppts_ast_weak H).
+    + exact (aufrontier_to_ppts_ast_weak H0).
+    + eapply meas_lift_mono; [|exact H1]. exact (aphead_rel_mono CIH).
+  - apply PPTSGTau.
+    + apply aufrontier_match_to_ppts_ast_match.
+      exact (aufrontier_match_sim_mono CIH H).
+    + exact (CIH _ _ H0).
+  - apply PPTSGProb.
+    + apply aufrontier_match_to_ppts_ast_match.
+      exact (aufrontier_match_sim_mono CIH H).
+    + eapply meas_lift_mono; [|exact H0]. intros x y Hxy. exact (CIH _ _ Hxy).
+  - exact (PPTSGTauL IHHstep).
+  - exact (PPTSGTauR IHHstep).
+Qed.
+
+Theorem ppts_guarded_to_auweak {R1 R2}
+    (RR : R1 -> R2 -> Prop) :
+  forall t1 t2,
+    @ppts_guarded_bisim E M MI MC MO R1 R2 RR t1 t2 ->
+    @auweak E M MI MC MO R1 R2 RR t1 t2.
+Proof.
+  unfold auweak. coinduction CH CIH.
+  intros t1 t2 Hrel. unfold fauweak, auweak_body; cbn.
+  pose proof (ppts_guarded_unfold Hrel) as Hstep.
+  induction Hstep.
+  - eapply AUWFrontier.
+    + exact (ppts_ast_weak_to_aufrontier H).
+    + exact (ppts_ast_weak_to_aufrontier H0).
+    + eapply meas_lift_mono; [|exact H1]. exact (aphead_rel_mono CIH).
+  - apply AUWTau.
+    + apply ppts_ast_match_to_aufrontier_match.
+      exact (ppts_ast_match_mono CIH H).
+    + exact (CIH _ _ H0).
+  - apply AUWProb.
+    + apply ppts_ast_match_to_aufrontier_match.
+      exact (ppts_ast_match_mono CIH H).
+    + eapply meas_lift_mono; [|exact H0]. intros x y Hxy. exact (CIH _ _ Hxy).
+  - exact (AUWTauL IHHstep).
+  - exact (AUWTauR IHHstep).
+Qed.
+
+Theorem ppts_guarded_iff_auweak {R1 R2}
+    (RR : R1 -> R2 -> Prop) (t1 : ptree E M R1) (t2 : ptree E M R2) :
+  ppts_guarded_bisim RR t1 t2 <-> auweak RR t1 t2.
+Proof.
+  split; [apply ppts_guarded_to_auweak|apply auweak_to_ppts_guarded].
+Qed.
+
+(** Productive weak-only PTS bisimulation coincides with the guarded/raw
+    semantics.  Without [productive], [ppts_bisim] deliberately quotients
+    all states that expose no AST weak transition. *)
+Corollary ppts_bisim_productive_iff_guarded {R1 R2}
+    (RR : R1 -> R2 -> Prop)
+    (productive : forall u v,
+      @ppts_bisim E M MI MC MO R1 R2 RR u v ->
+      exists hs, ppts_ast_weak (observe u) hs)
+    (t1 : ptree E M R1) (t2 : ptree E M R2) :
+  ppts_bisim RR t1 t2 -> ppts_guarded_bisim RR t1 t2.
+Proof.
+  intro H. apply auweak_to_ppts_guarded.
+  exact (ppts_bisim_complete_productive productive H).
+Qed.
+
+End GuardedPTSCharacterization.
