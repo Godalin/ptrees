02c153c1d44ef6cc7250be1ed546c2d4bfa7ef59
Linyu Yang <yly1228@foxmail.com>
2026-08-15 08:48:41 +0800

    remove legacy structural Bernoulli normalization

--- theories/Examples/OperationalBernoulliFactory.v
+++ theories/Examples/OperationalBernoulliFactory.v
@@ -10,8 +10,8 @@ From PTree.Core Require Import PTreeDefinitionNew.
 From PTree.Prob Require Import RatSubTypes DiscreteMC EnumBindFacts
   MeasureIteration MeasureIterationEnum TwoLevelMeasure TwoLevelMeasureEnum
   FreeOmegaMeasure EnumMap.
-From PTree.Eq Require Import ShallowNew PStrong
-  UnifiedFrontier UnifiedPWeak PrimitiveStableHitting OperationalProbabilisticPTS
+From PTree.Eq Require Import ShallowNew UnifiedFrontier UnifiedPWeak
+  PrimitiveStableHitting OperationalProbabilisticPTS
   OperationalProbabilisticPTSFreeOmega.
 From PTree.Examples Require Import VonNeumannUnbounded RationalBernoulli
   BernoulliFactory.
@@ -34,70 +34,7 @@ Definition factoryE_no_event : forall X, factoryE X -> False :=
 Section FactoryOperationalNormalization.
 Variables pfalse ptrue : nnQ.
 
-Definition operational_factory_pair_sample :
-    ptree factoryE Enum (bool * bool) :=
-  Prob (factory_biased_coin pfalse ptrue) (fun b1 =>
-    Prob (factory_biased_coin pfalse ptrue) (fun b2 => Ret (b1, b2))).
-
-Definition operational_factory_pair_round
-    (_ : unit) (pair : bool * bool) : unit + bool :=
-  let '(b1, b2) := pair in vn_round_result b1 b2.
-
-Definition operational_factory_pair_step (_ : unit) :
-    ptree factoryE Enum (unit + bool) :=
-  PTree.bind operational_factory_pair_sample (fun pair =>
-    Ret (operational_factory_pair_round tt pair)).
-
-Definition operational_factory_pair_fair : ptree factoryE Enum bool :=
-  PTree.iter operational_factory_pair_step tt.
-
-Lemma factory_vn_step_pair_structural :
-  pstructural eq (factory_vn_step pfalse ptrue tt)
-    (operational_factory_pair_step tt).
-Proof.
-  unfold factory_vn_step, operational_factory_pair_step,
-    operational_factory_pair_sample.
-  apply pstructural_fold. rewrite observe_bind. cbn.
-  constructor=> b1.
-  apply pstructural_fold. rewrite observe_bind. cbn.
-  constructor=> b2.
-  apply observe_eq_pstructural. rewrite observe_bind. reflexivity.
-Qed.
-
-Theorem factory_fair_coin_pair_structural :
-  pstructural eq (factory_fair_coin pfalse ptrue)
-    operational_factory_pair_fair.
-Proof.
-  unfold factory_fair_coin, operational_factory_pair_fair.
-  apply pstructural_iter. intros [].
-  apply factory_vn_step_pair_structural.
-Qed.
-
-Lemma factory_fair_coin_pair_hitting fuel :
-  free_omega_lift eq
-    (operational_hitting_approx (MF := MF) fuel
-      (observe (factory_fair_coin pfalse ptrue)))
-    (operational_hitting_approx (MF := MF) fuel
-      (observe operational_factory_pair_fair)).
-Proof.
-  apply free_operational_hitting_pstructural_no_event.
-  - exact factoryE_no_event.
-  - apply factory_fair_coin_pair_structural.
-Qed.
-
 Local Notation factory_head A := (frontier_head factoryE Enum A).
-
-Definition operational_factory_pair_heads : MF (factory_head (bool * bool)) :=
-  @mixed_bind Enum MF FreeOmegaMixedMeasureInterface bool _
-    (factory_biased_coin pfalse ptrue) (fun b1 =>
-      @mixed_bind Enum MF FreeOmegaMixedMeasureInterface bool _
-        (factory_biased_coin pfalse ptrue) (fun b2 =>
-          FORet (FHRet (b1, b2)))).
-
-Definition operational_factory_pair_measure : Enum (bool * bool) :=
-  bind_Enum (factory_biased_coin pfalse ptrue) (fun b1 =>
-    bind_Enum (factory_biased_coin pfalse ptrue) (fun b2 =>
-      ret_Enum (b1, b2))).
 
 Polymorphic Definition operational_factory_head_value {X}
     (h : factory_head X) : X :=
@@ -105,68 +42,6 @@ Polymorphic Definition operational_factory_head_value {X}
   | FHRet x => x
   | @FHVis _ _ _ Y e _ => False_rect X (factoryE_no_event e)
   end.
-
-Lemma operational_factory_pair_ret_weak (x : bool * bool) :
-  @operational_weak factoryE Enum MF
-    (FreeOmegaObservableSemanticMeasureInterface
-      (NI := Enum_SemanticMeasureInterface)
-      (NO := Enum_SemanticOmegaInterface))
-    FreeOmegaMixedMeasureInterface
-    FreeOmegaObservableSemanticOmegaInterface (bool * bool)
-    (observe (Ret x)) (FORet (FHRet x)).
-Proof.
-  assert (Hobserve : observe
-    (Ret x : ptree factoryE Enum (bool * bool)) = RetF x)
-    by reflexivity.
-  rewrite Hobserve. apply (operational_weak_ret
-    (FI := FreeOmegaObservableSemanticMeasureInterface)
-    (FO := FreeOmegaObservableSemanticOmegaInterface)
-    (MX := FreeOmegaMixedMeasureInterface) (E := factoryE)).
-Qed.
-
-Lemma operational_factory_pair_heads_observes :
-  free_omega_observes operational_factory_head_value
-    operational_factory_pair_heads operational_factory_pair_measure.
-Proof.
-  unfold operational_factory_pair_heads, operational_factory_pair_measure.
-  change (free_omega_observes operational_factory_head_value
-    (FOSample (factory_biased_coin pfalse ptrue) (fun b1 =>
-      FOSample (factory_biased_coin pfalse ptrue) (fun b2 =>
-        FORet (FHRet (b1, b2)))))
-    (@sem_bind Enum Enum_SemanticMeasureInterface _ _
-      (factory_biased_coin pfalse ptrue) (fun b1 =>
-        @sem_bind Enum Enum_SemanticMeasureInterface _ _
-          (factory_biased_coin pfalse ptrue) (fun b2 =>
-            @sem_ret Enum Enum_SemanticMeasureInterface _ (b1, b2))))).
-  eapply FOOObserveSample. intro b1.
-  eapply FOOObserveSample. intro b2. constructor.
-Qed.
-
-Lemma operational_factory_pair_sample_weak :
-  @operational_weak factoryE Enum MF
-    (FreeOmegaObservableSemanticMeasureInterface
-      (NI := Enum_SemanticMeasureInterface)
-      (NO := Enum_SemanticOmegaInterface))
-    FreeOmegaMixedMeasureInterface
-    FreeOmegaObservableSemanticOmegaInterface (bool * bool)
-    (observe operational_factory_pair_sample)
-    operational_factory_pair_heads.
-Proof.
-  unfold operational_factory_pair_sample, operational_factory_pair_heads.
-  eapply operational_weak_prob with (Good := fun _ => True).
-  - apply sem_ae_true.
-  - intros b1 _. eapply operational_weak_prob with (Good := fun _ => True).
-    + apply sem_ae_true.
-    + intros b2 _. apply operational_factory_pair_ret_weak.
-Qed.
-
-Definition operational_factory_fair_row (outer : nat) :
-    MF (factory_head bool) :=
-  free_nested_row_out factoryE_no_event operational_factory_pair_round
-    operational_factory_pair_heads outer tt.
-
-Definition operational_factory_fair_heads : MF (factory_head bool) :=
-  FOLub operational_factory_fair_row.
 
 Fixpoint operational_factory_fair_measure_row (outer : nat) : Enum bool :=
   match outer with
@@ -213,46 +88,6 @@ Proof.
   - rewrite /ret_Enum /bind_Enum /=.
     change (ret_Enum b = (scale_Enum 1 (ret_Enum b) ++ nil)%list).
     rewrite scale_Enum_one. symmetry. apply enum_cat_nil.
-Qed.
-
-Lemma operational_factory_fair_row_observes outer :
-  free_omega_observes operational_factory_head_value
-    (operational_factory_fair_row outer)
-    (operational_factory_fair_measure_row outer).
-Proof.
-  induction outer as [|outer IH].
-  - constructor.
-  - unfold operational_factory_fair_row.
-    cbn [free_nested_row_out operational_factory_pair_heads
-      operational_factory_pair_round free_no_event_head_value
-      free_no_event_head_value_for
-      free_omega_bind mixed_bind FreeOmegaMixedMeasureInterface
-      meas_iter_approx].
-    cbn [operational_factory_fair_measure_row
-      operational_factory_pair_round operational_factory_head_value
-      free_no_event_head_value].
-    change (free_omega_observes operational_factory_head_value
-      (FOSample (factory_biased_coin pfalse ptrue) (fun b1 =>
-        FOSample (factory_biased_coin pfalse ptrue) (fun b2 =>
-          match vn_round_result b1 b2 with
-          | inl i' => free_nested_row_out factoryE_no_event
-              operational_factory_pair_round operational_factory_pair_heads
-              outer i'
-          | inr b => FORet (FHRet b)
-          end)))
-      (@sem_bind Enum Enum_SemanticMeasureInterface _ _
-        (factory_biased_coin pfalse ptrue) (fun b1 =>
-          @sem_bind Enum Enum_SemanticMeasureInterface _ _
-            (factory_biased_coin pfalse ptrue) (fun b2 =>
-              match vn_round_result b1 b2 with
-              | inl _ => operational_factory_fair_measure_row outer
-              | inr b => @sem_ret Enum Enum_SemanticMeasureInterface _ b
-              end)))).
-    eapply FOOObserveSample. intro b1.
-    eapply FOOObserveSample. intro b2.
-    destruct (vn_round_result b1 b2) as [u|b].
-    + destruct u. exact IH.
-    + constructor.
 Qed.
 
 Definition operational_factory_raw_after (next : unit + bool) :
