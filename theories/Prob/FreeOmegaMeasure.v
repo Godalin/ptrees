7264c073383b309e11021d1596348bc0018e1cec
Linyu Yang <yly1228@foxmail.com>
2026-08-15 22:48:44 +0800

    feat(prob): make observable FreeOmega omega-AE safe

--- theories/Prob/FreeOmegaMeasure.v
+++ theories/Prob/FreeOmegaMeasure.v
@@ -2,7 +2,7 @@ Set Warnings "-notation-overridden".
 Set Warnings "-ambiguous-paths".
 Set Universe Polymorphism.
 
-Require Import FunctionalExtensionality Program.Equality Morphisms.
+Require Import FunctionalExtensionality Program.Equality Morphisms Arith.
 
 From PTree.Prob Require Import TwoLevelMeasure.
 
@@ -410,6 +410,18 @@ Proof.
   - exact (free_omega_approx_comp (R := eq) (T := eq) Hmn Hnx).
 Qed.
 
+Lemma free_omega_approx_steps {A} (c : nat -> FreeOmega MN A) :
+  (forall n, free_omega_approx eq (c n) (c (S n))) ->
+  forall n k, free_omega_approx eq (c n) (c (n + k)).
+Proof.
+  intros Hinc n k. induction k.
+  - replace (n + 0) with n by exact (plus_n_O n).
+    apply free_omega_approx_refl. intros x. exact eq_refl.
+  - replace (n + S k) with (S (n + k)) by
+      exact (plus_n_Sm n k).
+    eapply free_omega_approx_trans; eauto.
+Qed.
+
 Lemma free_omega_lift_sym {A B} (R : A -> B -> Prop) mu nu :
   free_omega_lift R mu nu ->
   free_omega_lift (fun y x => R x y) nu mu.
@@ -487,6 +499,34 @@ Proof.
   - intros A P x Hx. constructor. exact Hx.
   - intros A B mu k P Q Hmu Hk.
     cbn in Hmu, Hk |- *. exact (free_omega_ae_bind Hmu Hk).
+Qed.
+
+Lemma free_omega_ae_countable
+    `{NCountAE : @SemanticMeasureCountableAELaws MN NI}
+    {A} (mu : FreeOmega MN A) (P : nat -> A -> Prop) :
+  (forall n, free_omega_ae (P n) mu) ->
+  free_omega_ae (fun x => forall n, P n x) mu.
+Proof.
+  revert P. induction mu as [x| |X node k IH|chain IH]; intros P HP.
+  - constructor. intro n. specialize (HP n). dependent destruction HP.
+    assumption.
+  - constructor.
+  - eapply FOAESample with
+      (Good := fun x => forall n, free_omega_ae (P n) (k x)).
+    + apply sem_ae_countable. intro n.
+      specialize (HP n). dependent destruction HP.
+      eapply sem_ae_mono; [|eassumption]. intros y Hy. eauto.
+    + intros x Hx. apply IH. exact Hx.
+  - constructor. intro m. apply IH. intro n. specialize (HP n).
+    dependent destruction HP. eauto.
+Qed.
+
+#[global] Polymorphic Instance FreeOmegaSemanticMeasureCountableAELaws
+    `{NCountAE : @SemanticMeasureCountableAELaws MN NI} :
+    @SemanticMeasureCountableAELaws (FreeOmega MN)
+      (FreeOmegaSemanticMeasureInterface (NI := NI)).
+Proof.
+  constructor. intros A mu P HP. apply free_omega_ae_countable. exact HP.
 Qed.
 
 Lemma free_omega_lift_ae_restrict
@@ -796,6 +836,35 @@ Proof.
     exists z. split; [exists y; split|]; assumption.
 Qed.
 
+Lemma free_omega_support_lift_restrict {MN}
+    `{NI : SemanticMeasureInterface MN}
+    `{NC : @SemanticMeasureCoreLaws MN NI}
+    {A B} (R : A -> B -> Prop) mu nu (P : A -> Prop) (Q : B -> Prop) :
+  free_omega_support_lift R mu nu ->
+  free_omega_ae P mu -> free_omega_ae Q nu ->
+  free_omega_support_lift (fun x y => R x y /\ P x /\ Q y) mu nu.
+Proof.
+  intros [Hright Hleft] HP HQ. split.
+  - intros P0 HP0.
+    pose proof (free_omega_ae_conj (P := P0) (Q := P)
+      (mu := mu) HP0 HP) as HPboth.
+    pose proof (Hright _ HPboth) as Himage.
+    pose proof (free_omega_ae_conj (P := _ ) (Q := Q)
+      (mu := nu) Himage HQ) as Hboth.
+    eapply free_omega_ae_mono; [|exact Hboth].
+    intros y [[x [Hxy [HP0x HPx]]] HQy].
+    exists x. repeat split; assumption.
+  - intros Q0 HQ0.
+    pose proof (free_omega_ae_conj (P := Q0) (Q := Q)
+      (mu := nu) HQ0 HQ) as HQboth.
+    pose proof (Hleft _ HQboth) as Himage.
+    pose proof (free_omega_ae_conj (P := _) (Q := P)
+      (mu := mu) Himage HP) as Hboth.
+    eapply free_omega_ae_mono; [|exact Hboth].
+    intros x [[y [Hxy [HQ0y HQy]]] HPx].
+    exists y. repeat split; assumption.
+Qed.
+
 Lemma free_omega_support_lift_bind {MN}
     `{NI : SemanticMeasureInterface MN}
     `{NC : @SemanticMeasureCoreLaws MN NI}
@@ -846,6 +915,50 @@ Proof.
       * eassumption.
     + intros x [y [Hxy Hy]].
       apply (proj2 (Hkh x y Hxy) Q). eauto.
+Qed.
+
+Lemma free_omega_ae_sample_inv {MN}
+    `{NI : SemanticMeasureInterface MN}
+    `{NC : @SemanticMeasureCoreLaws MN NI}
+    {A C} (mu : MN C) (k : C -> FreeOmega MN A) (P : A -> Prop) :
+  free_omega_ae P (FOSample mu k) ->
+  sem_ae mu (fun x => free_omega_ae P (k x)).
+Proof.
+  intro HP. dependent destruction HP.
+  eapply sem_ae_mono; [|eassumption]. intros x Hx. eauto.
+Qed.
+
+Lemma free_omega_support_lift_sample_lub {MN}
+    `{NI : SemanticMeasureInterface MN}
+    `{NC : @SemanticMeasureCoreLaws MN NI}
+    `{NCountAE : @SemanticMeasureCountableAELaws MN NI}
+    {A B C} (R : A -> B -> Prop) (mu : MN C) (Good : C -> Prop)
+    (chain : C -> nat -> FreeOmega MN B)
+    (out : C -> FreeOmega MN A) :
+  sem_ae mu Good ->
+  (forall x, Good x ->
+    free_omega_support_lift R (out x) (FOLub (chain x))) ->
+  free_omega_support_lift R
+    (FOSample mu out)
+    (FOLub (fun n => FOSample mu (fun x => chain x n))).
+Proof.
+  intros HGood Hout. split.
+  - intros P HP. apply free_omega_ae_sample_inv in HP. constructor. intro n.
+    eapply FOAESample with
+      (Good := fun x => Good x /\ free_omega_ae P (out x)).
+    + apply sem_ae_conj; assumption.
+    + intros x [HxGood HxP].
+      pose proof ((proj1 (Hout x HxGood)) P HxP) as Hlub.
+      dependent destruction Hlub. eauto.
+  - intros Q HQ. dependent destruction HQ.
+    eapply FOAESample with
+      (Good := fun x => Good x /\
+        forall n, free_omega_ae Q (chain x n)).
+    + apply sem_ae_conj; [exact HGood|].
+      apply sem_ae_countable. intro n.
+      apply free_omega_ae_sample_inv. eauto.
+    + intros x [HxGood HxQ].
+      apply (proj2 (Hout x HxGood) Q). constructor. exact HxQ.
 Qed.
 
 Lemma free_omega_support_lift_lub {MN}
@@ -912,6 +1025,99 @@ Proof.
     end.
 Qed.
 
+Lemma free_omega_support_lift_bind_diagonal {MN}
+    `{NI : SemanticMeasureInterface MN}
+    `{NC : @SemanticMeasureCoreLaws MN NI}
+    `{NCAE : @SemanticMeasureCouplingAELaws MN NI}
+    `{NCountAE : @SemanticMeasureCountableAELaws MN NI}
+    {A B} (R : B -> B -> Prop)
+    (source : nat -> FreeOmega MN A) (source_out : FreeOmega MN A)
+    (kernels : A -> nat -> FreeOmega MN B)
+    (kernel_out : A -> FreeOmega MN B) :
+  (forall n, free_omega_approx eq (source n) (source (S n))) ->
+  (forall x n,
+    free_omega_approx eq (kernels x n) (kernels x (S n))) ->
+  free_omega_support_lift eq source_out (FOLub source) ->
+  (forall x, free_omega_support_lift R
+    (kernel_out x) (FOLub (kernels x))) ->
+  free_omega_support_lift R
+    (free_omega_bind source_out kernel_out)
+    (FOLub (fun n => free_omega_bind (source n)
+      (fun x => kernels x n))).
+Proof.
+  intros Hsource_inc Hkernels_inc Hsource Hkernels. split.
+  - intros P HP. apply free_omega_ae_bind_inv in HP.
+    pose proof ((proj1 Hsource) _ HP) as HsourceP.
+    eapply free_omega_ae_mono in HsourceP.
+    2: { intros x [y [-> Hy]]. exact Hy. }
+    dependent destruction HsourceP. constructor. intro n.
+    eapply free_omega_ae_bind; [eauto|]. intros x Hx.
+    pose proof ((proj1 (Hkernels x)) P Hx) as HkernelP.
+    dependent destruction HkernelP. eauto.
+  - intros Q HQ. dependent destruction HQ.
+    apply free_omega_ae_bind with
+      (P := fun x => forall j, free_omega_ae Q (kernels x j)).
+    + eapply free_omega_ae_mono.
+      2: { apply (proj2 Hsource). constructor. intro i.
+        apply free_omega_ae_countable. intro j.
+      pose (fuel := i + j).
+      assert (Hdiag : free_omega_ae Q
+          (free_omega_bind (source fuel)
+            (fun x => kernels x fuel))) by eauto.
+      apply free_omega_ae_bind_inv in Hdiag.
+      pose proof (free_omega_approx_steps Hsource_inc i j) as Hsrc.
+      pose proof (free_omega_approx_ae_backward
+        (R := eq) Hsrc Hdiag) as Hsrc_ae.
+      eapply free_omega_ae_mono in Hsrc_ae.
+      2: { intros x [y [-> Hy]]. exact Hy. }
+      eapply free_omega_ae_mono; [|exact Hsrc_ae]. intros x Hfuel.
+      pose proof (free_omega_approx_steps (Hkernels_inc x) j i) as Hkernel.
+      replace (j + i) with fuel in Hkernel by
+        (unfold fuel; apply Nat.add_comm).
+      pose proof (free_omega_approx_ae_backward
+        (R := eq) Hkernel Hfuel) as Hj.
+      eapply free_omega_ae_mono; [|exact Hj].
+      intros y [z [-> Hz]]. exact Hz. }
+      intros x [y [-> Hy]]. exact Hy.
+    + intros x Hx. apply (proj2 (Hkernels x) Q). constructor. exact Hx.
+Qed.
+
+Lemma free_omega_support_lift_double_diagonal {MN}
+    `{NI : SemanticMeasureInterface MN}
+    `{NC : @SemanticMeasureCoreLaws MN NI}
+    `{NCAE : @SemanticMeasureCouplingAELaws MN NI}
+    {A} (grid : nat -> nat -> FreeOmega MN A) :
+  (forall outer inner,
+    free_omega_approx eq (grid outer inner) (grid outer (S inner))) ->
+  (forall outer inner,
+    free_omega_approx eq (grid outer inner) (grid (S outer) inner)) ->
+  free_omega_support_lift eq
+    (FOLub (fun outer => FOLub (grid outer)))
+    (FOLub (fun fuel => grid fuel fuel)).
+Proof.
+  intros Hrows Hcols. split.
+  - intros P HP. dependent destruction HP. constructor. intro fuel.
+    match goal with
+    | Houter : forall i : nat, _ |- _ =>
+      specialize (Houter fuel); dependent destruction Houter
+    end.
+    eapply free_omega_ae_mono; [|eauto].
+    intros x Hx. exists x. split; [reflexivity|exact Hx].
+  - intros P HP. dependent destruction HP. constructor. intro outer.
+    constructor. intro inner. pose (fuel := outer + inner).
+    assert (Hfuel : free_omega_ae P (grid fuel fuel)) by eauto.
+    pose proof (free_omega_approx_steps (Hrows outer) inner outer) as Hrow.
+    replace (inner + outer) with fuel in Hrow by
+      (unfold fuel; apply Nat.add_comm).
+    pose proof (free_omega_approx_steps
+      (fun n => Hcols n fuel) outer inner) as Hcol.
+    pose proof (free_omega_approx_trans Hrow Hcol) as Happrox.
+    pose proof (free_omega_approx_ae_backward
+      (R := eq) Happrox Hfuel) as Hback.
+    eapply free_omega_ae_mono; [|exact Hback].
+    intros x [y [-> Hy]]. exists y. split; [reflexivity|exact Hy].
+Qed.
+
 (** An observation-closed coupling for the free omega completion.  The
     structural lifting above remains useful for syntax-directed proofs, but
     it deliberately cannot identify, for example, a formal omega limit with
@@ -933,6 +1139,13 @@ Polymorphic Inductive free_omega_qlift {MN}
       sem_lift S outA outB ->
       (forall x y, S (obsA x) (obsB y) -> R x y) ->
       free_omega_support_lift R mu nu ->
+      free_omega_qlift R mu nu
+  | FOQLAERestrict (T : A -> B -> Prop)
+      (mu : FreeOmega MN A) (nu : FreeOmega MN B)
+      (P : A -> Prop) (Q : B -> Prop) :
+      free_omega_qlift T mu nu ->
+      free_omega_ae P mu -> free_omega_ae Q nu ->
+      (forall x y, T x y /\ P x /\ Q y -> R x y) ->
       free_omega_qlift R mu nu
   | FOQLMono (T : A -> B -> Prop) mu nu :
       free_omega_qlift T mu nu ->
@@ -995,6 +1208,10 @@ Polymorphic Inductive free_omega_qlift {MN}
       (kernel_out : C -> FreeOmega MN A) :
       free_omega_qlift eq source_out (FOLub source) ->
       (forall x, free_omega_qlift R (kernel_out x) (FOLub (kernels x))) ->
+      free_omega_support_lift R
+        (free_omega_bind source_out kernel_out)
+        (FOLub (fun n => free_omega_bind (source n)
+          (fun x => kernels x n))) ->
       free_omega_qlift R
         (free_omega_bind source_out kernel_out)
         (FOLub (fun n => free_omega_bind (source n)
@@ -1008,6 +1225,10 @@ Polymorphic Inductive free_omega_qlift {MN}
         free_omega_approx eq (grid outer inner)
           (grid (Datatypes.S outer) inner)) ->
       (forall x, R x (eq_rect A (fun T => T) x B HAB)) ->
+      free_omega_support_lift R
+        (FOLub (fun outer => FOLub (grid outer)))
+        (eq_rect A (fun T => FreeOmega MN T)
+          (FOLub (fun fuel => grid fuel fuel)) B HAB) ->
       free_omega_qlift R
         (FOLub (fun outer => FOLub (grid outer)))
         (eq_rect A (fun T => FreeOmega MN T)
@@ -1016,6 +1237,48 @@ Polymorphic Inductive free_omega_qlift {MN}
       (right : nat -> FreeOmega MN B) :
       free_omega_chains_cofinal R left right ->
       free_omega_qlift R (FOLub left) (FOLub right).
+
+Theorem free_omega_qlift_support {MN}
+    `{NI : SemanticMeasureInterface MN}
+    `{NC : @SemanticMeasureCoreLaws MN NI}
+    `{NCAE : @SemanticMeasureCouplingAELaws MN NI}
+    `{NCountAE : @SemanticMeasureCountableAELaws MN NI}
+    `{NO : @SemanticOmegaInterface MN NI}
+    {A B} (R : A -> B -> Prop) mu nu :
+  @free_omega_qlift MN NI NO A B R mu nu ->
+  free_omega_support_lift R mu nu.
+Proof.
+  intro Hq. induction Hq.
+  - apply free_omega_lift_support_lift. assumption.
+  - assumption.
+  - eapply free_omega_support_lift_mono.
+    + eapply free_omega_support_lift_restrict; eauto.
+    + eauto.
+  - eapply free_omega_support_lift_mono; eauto.
+  - apply free_omega_support_lift_sym. assumption.
+  - eapply free_omega_support_lift_mono.
+    + eapply free_omega_support_lift_comp; eassumption.
+    + assumption.
+  - eapply free_omega_support_lift_bind; eauto.
+  - eapply free_omega_support_lift_sample; eauto.
+  - apply free_omega_support_lift_lub. assumption.
+  - apply free_omega_support_lift_lub_zero_prefix_l. assumption.
+  - apply free_omega_support_lift_sym.
+    apply free_omega_support_lift_lub_zero_prefix_l.
+    intro n. apply free_omega_support_lift_sym. auto.
+  - eapply free_omega_support_lift_sample_lub; eauto.
+  - apply free_omega_support_lift_sample_zero.
+  - apply free_omega_support_lift_lub_constant_r. assumption.
+  - assumption.
+  - assumption.
+  - destruct H as [Hlr Hrl]. split.
+    + intros P HP. dependent destruction HP. constructor. intro m.
+      destruct (Hrl m) as [n Happrox].
+      eapply free_omega_approx_ae_backward; [exact Happrox|]. eauto.
+    + intros Q HQ. dependent destruction HQ. constructor. intro n.
+      destruct (Hlr n) as [m Happrox].
+      eapply free_omega_approx_ae_backward; [exact Happrox|]. eauto.
+Qed.
 
 #[global] Polymorphic Instance FreeOmegaObservableSemanticMeasureInterface
     {MN} `{NI : SemanticMeasureInterface MN}
@@ -1096,6 +1359,31 @@ Proof.
   - intros A P x Hx. constructor. exact Hx.
   - intros A B mu k P Q Hmu Hk.
     cbn in Hmu, Hk |- *. exact (free_omega_ae_bind Hmu Hk).
+Qed.
+
+#[global] Instance FreeOmegaObservableSemanticMeasureCountableAELaws
+    `{NCountAE : @SemanticMeasureCountableAELaws MN NI} :
+    @SemanticMeasureCountableAELaws (FreeOmega MN)
+      (FreeOmegaObservableSemanticMeasureInterface (NI := NI) (NO := NO)).
+Proof.
+  constructor. intros A mu P HP. apply free_omega_ae_countable. exact HP.
+Qed.
+
+#[global] Instance FreeOmegaObservableSemanticMeasureCouplingAELaws
+    `{NCAE : @SemanticMeasureCouplingAELaws MN NI}
+    `{NCountAE : @SemanticMeasureCountableAELaws MN NI} :
+    @SemanticMeasureCouplingAELaws (FreeOmega MN)
+      (FreeOmegaObservableSemanticMeasureInterface (NI := NI) (NO := NO)).
+Proof.
+  constructor.
+  - intros A B R mu nu P Hlift HP.
+    exact (proj1 (free_omega_qlift_support Hlift) P HP).
+  - intros A B R mu nu P Q Hlift HP HQ.
+    eapply FOQLAERestrict with (T := R) (P := P) (Q := Q).
+    + exact Hlift.
+    + exact HP.
+    + exact HQ.
+    + intros x y Hxy. exact Hxy.
 Qed.
 
 Lemma free_omega_qlift_bind_ae
@@ -1223,6 +1511,24 @@ Proof.
     intros z. reflexivity.
 Qed.
 
+#[global] Instance FreeOmegaObservableSemanticOmegaAELaws
+    `{NCAE : @SemanticMeasureCouplingAELaws MN NI}
+    `{NCountAE : @SemanticMeasureCountableAELaws MN NI} :
+    @SemanticOmegaAELaws (FreeOmega MN)
+      (FreeOmegaObservableSemanticMeasureInterface (NI := NI) (NO := NO))
+      FreeOmegaObservableSemanticOmegaInterface.
+Proof.
+  constructor.
+  - intros A P. constructor.
+  - intros A chain out P Hlub HP. cbn in Hlub.
+    pose proof (free_omega_qlift_support Hlub) as Hsupport.
+    assert (Hchain : free_omega_ae P (FOLub chain)).
+    { constructor. exact HP. }
+    pose proof ((proj2 Hsupport) P Hchain) as Himage.
+    eapply free_omega_ae_mono; [|exact Himage].
+    intros x [y [Hxy Hy]]. subst y. exact Hy.
+Qed.
+
 #[global] Instance FreeOmegaObservableSemanticOmegaCofinalityLaws :
     @SemanticOmegaCofinalityLaws (FreeOmega MN)
       (FreeOmegaObservableSemanticMeasureInterface (NI := NI) (NO := NO))
@@ -1257,17 +1563,24 @@ Proof.
     eapply FOQLSampleLub; eauto.
 Qed.
 
-#[global] Instance FreeOmegaObservableSemanticMeasureDiagonalLaws :
+#[global] Instance FreeOmegaObservableSemanticMeasureDiagonalLaws
+    `{NCAE : @SemanticMeasureCouplingAELaws MN NI}
+    `{NCountAE : @SemanticMeasureCountableAELaws MN NI} :
     @SemanticMeasureDiagonalLaws (FreeOmega MN)
       (FreeOmegaObservableSemanticMeasureInterface (NI := NI) (NO := NO))
       FreeOmegaObservableSemanticOmegaInterface.
 Proof.
-  constructor. intros A B source source_out kernels kernel_out _ _ Hsource Hkernels.
+  constructor. intros A B source source_out kernels kernel_out
+    Hsource_inc Hkernels_inc Hsource Hkernels.
   cbn in Hsource, Hkernels |- *.
-  eapply FOQLBindLub; eauto.
+  eapply FOQLBindLub; [exact Hsource|exact Hkernels|].
+  eapply free_omega_support_lift_bind_diagonal; eauto.
+  - apply free_omega_qlift_support. exact Hsource.
+  - intro x. apply free_omega_qlift_support. exact (Hkernels x).
 Qed.
 
-#[global] Instance FreeOmegaObservableSemanticOmegaFubiniLaws :
+#[global] Instance FreeOmegaObservableSemanticOmegaFubiniLaws
+    `{NCAE : @SemanticMeasureCouplingAELaws MN NI} :
     @SemanticOmegaFubiniLaws (FreeOmega MN)
       (FreeOmegaObservableSemanticMeasureInterface (NI := NI) (NO := NO))
       FreeOmegaObservableSemanticOmegaInterface.
@@ -1285,6 +1598,9 @@ Proof.
     + intros outer inner. exact (Hrows_inc outer inner).
     + intros outer inner. exact (Hcols_inc inner outer).
     + intros x. reflexivity.
+    + apply free_omega_support_lift_double_diagonal.
+      * intros outer inner. exact (Hrows_inc outer inner).
+      * intros outer inner. exact (Hcols_inc inner outer).
 Qed.
 
 #[global] Instance FreeOmegaObservableSemanticMeasureOrderLaws :
